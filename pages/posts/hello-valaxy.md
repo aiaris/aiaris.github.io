d13fd224a8d32b739f54f2d9d5c7cb99437f13d1
aiaris <15129404526@163.com>
2024-05-23 00:39:10 +0800

    风信子的博客

---
+++ pages/posts/hello-valaxy.md
@@ -0,0 +1,20 @@
+---
+title: 我的下头群友们!
+date: 2024-05-22
+updated: 2024-05-23
+categories: Valaxy 笔记
+tags:
+  - valaxy
+  - 笔记
+top: 1
+---
+
+## Valaxy
+
+赞美叮当
+
+Write your first post!
+
+## Usage
+
+Modify `valaxy.config.ts` to custom your blog.
---
+++ site.config.ts
@@ -0,0 +1,127 @@
+import { defineSiteConfig } from 'valaxy'
+
+export default defineSiteConfig({
+  url: 'https://valaxy.site/',
+  lang: 'zh-CN',
+  title: '蓝铃轻语,风信绘梦',
+  author: {
+    name: '风信子',
+  },
+  description: '落日沉溺于橘色的海,晚风沦陷于赤城的爱',
+  social: [
+    {
+      name: 'RSS',
+      link: '/atom.xml',
+      icon: 'i-ri-rss-line',
+      color: 'orange',
+    },
+    {
+      name: 'QQ 1599669295',
+      link: 'https://tool.gljlw.com/qqq/?qq=1599669295',
+      icon: 'i-ri-qq-line',
+      color: '#12B7F5',
+    },
+    {
+      name: 'GitHub',
+      link: 'https://github.com/aiaris',
+      icon: 'i-ri-github-line',
+      color: '#6e5494',
+    },
+    // {
+    //   name: '微博',
+    //   link: 'https://weibo.com/jizhideyunyoujun',
+    //   icon: 'i-ri-weibo-line',
+    //   color: '#E6162D',
+    // },
+    // {
+    //   name: '豆瓣',
+    //   link: 'https://www.douban.com/people/yunyoujun/',
+    //   icon: 'i-ri-douban-line',
+    //   color: '#007722',
+    // },
+    // {
+    //   name: '网易云音乐',
+    //   link: 'https://music.163.com/#/user/home?id=247102977',
+    //   icon: 'i-ri-netease-cloud-music-line',
+    //   color: '#C20C0C',
+    // },
+    {
+      name: '知乎',
+      link: 'https://www.zhihu.com/people/aiaris-33',
+      icon: 'i-ri-zhihu-line',
+      color: '#0084FF',
+    },
+    {
+      name: '哔哩哔哩',
+      link: 'https://space.bilibili.com/20569463?spm_id_from=333.1007.0.0',
+      icon: 'i-ri-bilibili-line',
+      color: '#FF8EB3',
+    },
+    {
+      name: '微信',
+      link: '/mingpian.jpg',
+      icon: 'i-ri-wechat-2-line',
+      color: '#1AAD19',
+    },
+    // {
+    //   name: 'Twitter',
+    //   link: 'https://twitter.com/YunYouJun',
+    //   icon: 'i-ri-twitter-line',
+    //   color: '#1da1f2',
+    // },
+    // {
+    //   name: 'Telegram Channel',
+    //   link: 'https://t.me/elpsycn',
+    //   icon: 'i-ri-telegram-line',
+    //   color: '#0088CC',
+    // },
+    {
+      name: 'E-Mail 15129404526@163.com',
+      link: '/',
+      icon: 'i-ri-mail-line',
+      color: '#8E71C1',
+    },
+    // {
+    //   name: 'Travelling',
+    //   link: 'https://www.travellings.cn/go.html',
+    //   icon: 'i-ri-train-line',
+    //   color: 'var(--va-c-text)',
+    // },
+  ],
+
+  search: {
+    enable: false,
+  },
+
+  /**
+   * Default Frontmatter
+   */
+  frontmatter: {
+    time_warning: false,
+  },
+
+  sponsor: {
+    enable: true,
+    title: '我很可爱，请给我钱！',
+    methods: [
+      {
+        name: '支付宝',
+        url: '/zfbsq.jpg',
+        color: '#00A3EE',
+        icon: 'i-ri-alipay-line',
+      },
+      {
+        name: 'QQ 支付',
+        url: '/qqsq.png',
+        color: '#12B7F5',
+        icon: 'i-ri-qq-line',
+      },
+      {
+        name: '微信支付',
+        url: '/wxsq.jpg',
+        color: '#2DC100',
+        icon: 'i-ri-wechat-pay-line',
+      },
+    ],
+  },
+})
