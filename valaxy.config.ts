d2cd07a8f6e58c1183ed9ac1e00eac6c9f8cc2d9
Shallow Dream <1599669295@qq.com>
2024-05-24 09:51:59 +0800

    评论

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -1,23 +1,22 @@
-import { defineValaxyConfig } from 'valaxy'
-import type { UserThemeConfig } from 'valaxy-theme-yun'
+import { defineValaxyConfig } from "valaxy";
+import type { UserThemeConfig } from "valaxy-theme-yun";
+import { addonWaline } from "valaxy-addon-waline";
 
 // add icons what you will need
-const safelist = [
-  'i-ri-home-line',
-]
+const safelist = ["i-ri-home-line"];
 
 /**
  * User Config
  */
-export default defineValaxyConfig<UserThemeConfig>({
+export default defineValaxyConfig({
   // site config see site.config.ts
 
-  theme: 'yun',
+  theme: "yun",
 
   themeConfig: {
     banner: {
       enable: true,
-      title: '蓝铃轻语,风信绘梦',
+      title: "蓝铃轻语,风信绘梦",
       cloud: {
         enable: true,
       },
@@ -25,19 +24,32 @@ export default defineValaxyConfig<UserThemeConfig>({
 
     pages: [
       {
-        name: '我的小伙伴们',
-        url: '/links/',
-        icon: 'i-ri-genderless-line',
-        color: 'dodgerblue',
+        name: "我的小伙伴们",
+        url: "/links/",
+        icon: "i-ri-genderless-line",
+        color: "dodgerblue",
       },
       {
-        name: '喜欢的女孩子',
-        url: '/girls/',
-        icon: 'i-ri-women-line',
-        color: 'hotpink',
+        name: "喜欢的女孩子",
+        url: "/girls/",
+        icon: "i-ri-women-line",
+        color: "hotpink",
       },
     ],
-
+    // or write it in site.config.ts
+    siteConfig: {
+      // 启用评论
+      comment: {
+        enable: true,
+      },
+    },
+    // 设置 valaxy-addon-waline 配置项
+    addons: [
+      addonWaline({
+        // Waline 配置项，参考 https://waline.js.org/reference/client/props.html
+        serverURL: "https://your-waline-url",
+      }),
+    ],
     // footer: {
     //   since: 2016,
     //   beian: {
@@ -48,4 +60,4 @@ export default defineValaxyConfig<UserThemeConfig>({
   },
 
   unocss: { safelist },
-})
+});
