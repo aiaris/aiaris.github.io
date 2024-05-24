a53fc946b307944be51e1db8f707866df0d814de
Shallow Dream <1599669295@qq.com>
2024-05-24 13:43:20 +0800

    修改评论

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -8,11 +8,24 @@ const safelist = ["i-ri-home-line"];
 /**
  * User Config
  */
-export default defineValaxyConfig({
+export default defineValaxyConfig<UserThemeConfig>({
   // site config see site.config.ts
-
+  // or write it in site.config.ts
+  siteConfig: {
+    // 启用评论
+    comment: {
+      enable: true,
+    },
+  },
+  // 设置 valaxy-addon-waline 配置项
+  addons: [
+    addonWaline({
+      // Waline 配置项，参考 https://waline.js.org/reference/client/props.html
+      serverURL:
+        "https://aiaris-github-io-74vt-bub6cwn99-aiaris-projects.vercel.app/",
+    }),
+  ],
   theme: "yun",
-
   themeConfig: {
     banner: {
       enable: true,
@@ -36,20 +49,7 @@ export default defineValaxyConfig({
         color: "hotpink",
       },
     ],
-    // or write it in site.config.ts
-    siteConfig: {
-      // 启用评论
-      comment: {
-        enable: true,
-      },
-    },
-    // 设置 valaxy-addon-waline 配置项
-    addons: [
-      addonWaline({
-        // Waline 配置项，参考 https://waline.js.org/reference/client/props.html
-        serverURL: "https://aiaris-github-io-74vt-bub6cwn99-aiaris-projects.vercel.app/",
-      }),
-    ],
+
     // footer: {
     //   since: 2016,
     //   beian: {
